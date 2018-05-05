357619a441920127cc721b1872b53ec31b7ea156
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-05 06:15:28 +0300

    Added Orchestra Testbench service providers.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,16 @@
 class TestCase extends \Orchestra\Testbench\TestCase {
 
     /**
+     * Laravel Disposable Email Test Package Service Providers
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app) {
+        return ['Propaganistas\LaravelDisposableEmail\DisposableEmailServiceProvider'];
+    }
+
+    /**
      * Laravel Disposable Email Test Package Aliases
      *
      * @param \Illuminate\Foundation\Application $app
