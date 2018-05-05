628df38ec4b6a1f5e38717b65f8b169136cfb924
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-05 04:49:43 +0300

    Added Indisposable import and updated `isDisposable()` method name.

--- tests/DisposableEmailTest.php
+++ tests/DisposableEmailTest.php
@@ -1,12 +1,13 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Propaganistas\LaravelDisposableEmail\Validation\Indisposable;
 
 class DisposableEmailTest extends TestCase {
 
     /** @test */
     public function an_email_address_from_a_disposable_email_provider_should_be_detected() {
-        $this->assertFalse(Indisposable::hasDisposableDomain('test@gmail.com'));
+        $this->assertFalse(Indisposable::isDisposable('test@gmail.com'));
     }
 
 }
