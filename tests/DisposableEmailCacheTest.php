4a970e02215ec45500296fe78fb7adfa1e4cf952
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-05 04:21:23 +0300

    Corrected DisposableEmailCacheTest class name.

--- tests/DisposableEmailCacheTest.php
+++ tests/DisposableEmailCacheTest.php
@@ -3,7 +3,7 @@
 use Orchestra\Testbench\TestCase;
 use Propaganistas\LaravelDisposableEmail\Validation\Cache;
 
-class DisposableEmailTest extends TestCase {
+class DisposableEmailCacheTest extends TestCase {
 
     /** @test */
     public function we_should_be_able_to_fetch_the_remote_source() {
