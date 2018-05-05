81e8ff5834e9114f4b3d4f4df2af6dd92f47c988
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-05 04:46:46 +0300

    Ported over core IndisposableValidation functionality to dedicated class for better testability and reusability.

---
+++ src/Validation/Indisposable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Propaganistas\LaravelDisposableEmail\Validation;
+
+use Illuminate\Support\Str;
+use Propaganistas\LaravelDisposableEmail\Validation\Cache;
+
+class Indisposable {
+
+    /**
+     * Array of disposable email domains.
+     *
+     * @var array
+     */
+    protected static $domains = [];
+
+    /**
+     * Indisposable constructor.
+     */
+    public function __construct() {
+        static::$domains = Cache::fetchOrUpdate();
+    }
+
+    /**
+     * Checks whether or not the given email address' domain matches one from a disposable email service.
+     *
+     * @param $email
+     * @return bool
+     */
+    public static function isDisposable($email) {
+        // Parse the email to its top level domain.
+        preg_match("/[^\.\/]+\.[^\.\/]+$/", Str::after($email, '@'), $domain);
+
+        // Just ignore this validator if the value doesn't even resemble an email or domain.
+        if (count($domain) === 0) {
+            return true;
+        }
+
+        return in_array($domain[0], static::$domains);
+    }
+
+}
--- src/Validation/IndisposableValidation.php
+++ src/Validation/IndisposableValidation.php
@@ -2,33 +2,8 @@
 
 namespace Propaganistas\LaravelDisposableEmail\Validation;
 
-use Illuminate\Support\Str;
-
 class IndisposableValidation
 {
-    /**
-     * The array of disposable domains.
-     *
-     * @var array
-     */
-    protected static $domains = [];
-
-    /**
-     * Disposable constructor.
-     */
-    public function __construct()
-    {
-        if (empty(static::$domains)) {
-            $path = base_path('storage/framework/disposable_domains.json');
-
-            // If the list hasn't been fetched yet, fall back to defaults.
-            // This ensures developers are up and running right away.
-            static::$domains = json_decode(file_get_contents(
-                file_exists($path) ? $path : (__DIR__ . '/../../domains.json')
-            ), true);
-        }
-    }
-
     /**
      * Validates whether an email address does not originate from a disposable email service.
      *
@@ -40,14 +15,6 @@ class IndisposableValidation
      */
     public function validate($attribute, $value, $parameters, $validator)
     {
-        // Parse the email to its top level domain.
-        preg_match("/[^\.\/]+\.[^\.\/]+$/", Str::after($value, '@'), $domain);
-
-        // Just ignore this validator if the value doesn't even resemble an email or domain.
-        if (count($domain) === 0) {
-            return true;
-        }
-
-        return ! in_array($domain[0], static::$domains);
+        return ! Indisposable::isDisposable($value);
     }
 }
