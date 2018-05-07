55b45e62b6c565f0e6a53bbc5fb3e845a8ed2a8d
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-07 07:11:59 +0300

    Asserting that the Indisposable domain cache can be flushed.

--- tests/Validation/IndisposableTest.php
+++ tests/Validation/IndisposableTest.php
@@ -2,6 +2,7 @@
 
 namespace Propaganistas\LaravelDisposableEmail\Tests\Validation;
 
+use Illuminate\Support\Facades\Cache as FrameworkCache;
 use Propaganistas\LaravelDisposableEmail\Tests\TestCase;
 use Propaganistas\LaravelDisposableEmail\Facades\Indisposable;
 
@@ -23,6 +24,18 @@ class IndisposableTest extends TestCase {
     /** @test */
     public function a_commonly_known_disposable_email_provider_should_be_detected_as_disposable() {
         $this->assertTrue(Indisposable::isDisposable('test@yopmail.com'));
+    }
+
+    /** @test */
+    public function the_indisposable_remote_domains_cache_can_be_flushed() {
+        // Loads remote domains and caches them indefinitely.
+        Indisposable::remoteDomains();
+
+        $this->assertNotNull(FrameworkCache::get('laravel-disposable-email.cache'));
+
+        Indisposable::flushCache();
+
+        $this->assertNull(FrameworkCache::get('laravel-disposable-email.cache'));
     }
 
 }
