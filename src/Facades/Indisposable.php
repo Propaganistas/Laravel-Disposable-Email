2ae851c61101ef2627f255deca156b89cc343ce7
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-07 07:15:56 +0300

    Added IDE-helping PHPDoc to Indisposable facade.

--- src/Facades/Indisposable.php
+++ src/Facades/Indisposable.php
@@ -4,6 +4,15 @@ namespace Propaganistas\LaravelDisposableEmail\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Indisposable
+ *
+ * @method static boolean isDisposable()
+ * @method static array remoteDomains()
+ * @method static void flushCache()
+ *
+ * @package Propaganistas\LaravelDisposableEmail\Facades
+ */
 class Indisposable extends Facade {
 
     protected static function getFacadeAccessor() { return 'indisposable'; }
