938dfa89e01d3972079722f62d1f0085ef4e9b3e
Propaganistas <Propaganistas@users.noreply.github.com>
2020-06-22 08:27:29 +0200

    Update UpdateDisposableDomainsCommand.php

--- src/Console/UpdateDisposableDomainsCommand.php
+++ src/Console/UpdateDisposableDomainsCommand.php
@@ -13,14 +13,6 @@ class UpdateDisposableDomainsCommand extends Command
     use ParsesJson;
 
     /**
-     * The console command name.
-     * For Laravel 5.0
-     *
-     * @var string
-     */
-    protected $name = 'disposable:update';
-
-    /**
      * The name and signature of the console command.
      *
      * @var string
