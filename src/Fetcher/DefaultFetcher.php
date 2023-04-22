996dfd0a052765c0252a05325e171a0bececc6d6
Mexion <dev@mexion.com>
2023-04-22 14:54:06 +0200

    Use curl instead of file open as with file open the server could block this when the allow_url_open is set to false

--- src/Fetcher/DefaultFetcher.php
+++ src/Fetcher/DefaultFetcher.php
@@ -14,7 +14,7 @@ class DefaultFetcher implements Fetcher
             throw new InvalidArgumentException('Source URL is null');
         }
 
-        $content = file_get_contents($url);
+        $content = $this->file_get_contents_curl($url);
 
         if ($content === false) {
             throw new UnexpectedValueException('Failed to interpret the source URL ('.$url.')');
@@ -32,5 +32,17 @@ class DefaultFetcher implements Fetcher
         $data = json_decode($data, true);
 
         return json_last_error() === JSON_ERROR_NONE && ! empty($data);
+    }
+
+    public function file_get_contents_curl( $url ) {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $data;
     }
 }
